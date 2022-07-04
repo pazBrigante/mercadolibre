10297353ff09437859adfda444e0e3f16b88e147
pazBrigante <106492230+pazBrigante@users.noreply.github.com>
2022-07-04 20:19:01 -0300

    comienzo

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const express = require ('express');
+
+const path = require ('path');
+
+const app = express ();
+
+const publicPath = path.join(__dirname, '/public');
+
+app.use(express.static(publicPath))
+
+app.listen (3000, () =>{
+    console.log ("Servidor activo");
+});
+
+app.get ("/", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views/home.html"))
+});
+
+app.get ("/login", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views/login.html"))
+});
+
+app.get ("/", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views/register.html"))
+});
