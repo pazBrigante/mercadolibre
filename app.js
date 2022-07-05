cbd1ea4e3972e800a627e718f3cad0d77db364ac
pazBrigante <106492230+pazBrigante@users.noreply.github.com>
2022-07-04 21:10:19 -0300

    modificafion de puerto

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const publicPath = path.join(__dirname, '/public');
 
 app.use(express.static(publicPath))
 
-app.listen (3000, () =>{
+app.listen (process.env.PORT || 3000, () =>{
     console.log ("Servidor activo");
 });
 
